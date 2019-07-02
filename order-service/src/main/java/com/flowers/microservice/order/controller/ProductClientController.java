6c1c025ac8f8ebf648d9db4346e4133f4559c939
Casmon Gordon <48144124+journeymann@users.noreply.github.com>
2019-07-02 18:10:35 -0400

    Update ProductClientController.java

--- order-service/src/main/java/com/flowers/microservice/order/controller/ProductClientController.java
+++ order-service/src/main/java/com/flowers/microservice/order/controller/ProductClientController.java
@@ -5,13 +5,10 @@ package com.flowers.microservice.order.controller;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
