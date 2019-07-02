22b2679a911a2502a2c6796b6f86639bf4b48848
Casmon Gordon <48144124+journeymann@users.noreply.github.com>
2019-07-02 18:09:30 -0400

    Update ProductClientController.java
    adding circuit breaker

--- order-service/src/main/java/com/flowers/microservice/order/controller/ProductClientController.java
+++ order-service/src/main/java/com/flowers/microservice/order/controller/ProductClientController.java
@@ -5,10 +5,13 @@ package com.flowers.microservice.order.controller;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,7 @@ import com.flowers.microservice.order.model.Product;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import com.netflix.discovery.shared.Application;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 /**
  * @author cgordon
@@ -36,6 +40,7 @@ public class ProductClientController {
     @Value("${service.productsearch.serviceId}")
     private String productSearchServiceId;
     
+    @HystrixCommand(fallbackMethod = "fallbackProdById")
     @RequestMapping("/order/product/{productId}")
     public Product find(@PathVariable Long productId) {
         Application application = eurekaClient.getApplication(productSearchServiceId);
@@ -46,7 +51,8 @@ public class ProductClientController {
         System.out.printf("RESPONSE: %s\n",prod);
         return prod;
     }
-    
+        
+    @HystrixCommand(fallbackMethod = "fallbackAllProducts")
     @RequestMapping("/order/product/all")
     public Collection <Product> findProducts() {
         Application application = eurekaClient.getApplication(productSearchServiceId);
@@ -59,6 +65,13 @@ public class ProductClientController {
         return list;
     }
     
+    public Product fallbackProdById() {
+        return new Product();
+    }
+    
+    public Collection<Product> fallbackAllProducts() {
+        return new ArrayList<Product>();
+    }    
 	private final class ProductCollection extends ArrayList<Product>{
     	private static final long serialVersionUID = 7056106857753243257L;
 
