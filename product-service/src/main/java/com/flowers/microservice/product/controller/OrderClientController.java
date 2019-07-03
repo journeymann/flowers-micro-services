2fceebc6a52b708290fa53beb4d84f69b75a5306
Casmon Gordon <48144124+journeymann@users.noreply.github.com>
2019-07-03 10:28:38 -0400

    Update OrderClientController.java
    code refactor

--- product-service/src/main/java/com/flowers/microservice/product/controller/OrderClientController.java
+++ product-service/src/main/java/com/flowers/microservice/product/controller/OrderClientController.java
@@ -5,18 +5,11 @@ package com.flowers.microservice.product.controller;
 
 import java.util.ArrayList;
 import java.util.Collection;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import com.flowers.microservice.product.domain.Order;
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
-import com.netflix.discovery.shared.Application;
+import com.flowers.microservice.product.controller.AbstractClient;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 /**
@@ -25,53 +18,41 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
  * @version 1.0
  *
  */
-
-@RefreshScope
-@RestController
-public class OrderClientController {
+public class OrderClientController extends AbstractClient {
 	
-    @Autowired
-    private RestTemplate restTemplate;
-    @Autowired
-    private EurekaClient eurekaClient;
-    @Value("${service.ordersearch.serviceId}")
-    private String orderSearchServiceId;
+    @Value("${service.orderSearch.serviceId}")
+    private String searchServiceId;
     
-    @HystrixCommand(fallbackMethod = "fallback")
-    @RequestMapping("/product/order/{orderId}")
-    public Order find(@PathVariable Long productId) {
-        Application application = eurekaClient.getApplication(orderSearchServiceId);
-        InstanceInfo instanceInfo = application.getInstances().get(0);
-        String url = "http://" + instanceInfo.getIPAddr() + ":" + instanceInfo.getPort() + "/" + "order/read/" + productId;
-        System.out.printf("URL: %s\n",url);
-        Order prod = restTemplate.getForObject(url, Order.class);
-        System.out.printf("RESPONSE: %s\n",prod);
-        return prod;
+    private final String TYPE_KEY = "order";
+    
+    @HystrixCommand(fallbackMethod = "fallbackById")
+    @RequestMapping("/order/product/{Id}")
+    public Order find(@PathVariable Long Id) {
+
+        return (Order)super.find(Id);
     }
-    
-    @HystrixCommand(fallbackMethod = "fallbackAllOrders")
-    @RequestMapping("/order/product/all")
-    public Collection <Order> findOrders() {
-        Application application = eurekaClient.getApplication(orderSearchServiceId);
-        InstanceInfo instanceInfo = application.getInstances().get(0);
-        String url = "http://" + instanceInfo.getIPAddr() + ":" + instanceInfo.getPort() + "/" + "order/all";
-        System.out.printf("URL: %s\n", url);
         
-		Collection<Order> list =  restTemplate.getForObject(url, OrderCollection.class);
-        System.out.printf("RESPONSE: %s\n", list);
-        return list;
+    @HystrixCommand(fallbackMethod = "fallbackAll")
+    @RequestMapping("/order/product/all")
+    public Collection <Order> findProducts() {
+
+		return convertModelList(super.findAll(), Order.class);
     }
     
-    public Order fallback() {
+    public Order fallbackById() {
         return new Order();
     }
     
-    public Collection<Order> fallbackAllOrders() {
+    public Collection<Order> fallbackAll() {
         return new ArrayList<Order>();
-    }
+    }    
     
-	private final class OrderCollection extends ArrayList<Order>{
-    	private static final long serialVersionUID = 7056106857753243257L;
-
-    }
+    public final String getTypeKey() {
+    	return TYPE_KEY;
+    };
+    
+    public final String getSearchServiceId() {
+    	return searchServiceId;
+    };
+    
 }
