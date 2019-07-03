10aa2c82cde90b4d63c81744fe2ab217745d999e
Casmon Gordon <48144124+journeymann@users.noreply.github.com>
2019-07-03 10:24:49 -0400

    Update ProductClientController.java
    refactor

--- order-service/src/main/java/com/flowers/microservice/order/controller/ProductClientController.java
+++ order-service/src/main/java/com/flowers/microservice/order/controller/ProductClientController.java
@@ -5,18 +5,11 @@ package com.flowers.microservice.order.controller;
 
 import java.util.ArrayList;
 import java.util.Collection;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import com.flowers.microservice.order.model.Product;
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
-import com.netflix.discovery.shared.Application;
+import com.flowers.microservice.order.controller.AbstractClient;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 /**
@@ -25,54 +18,42 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
  * @version 1.0
  *
  */
-
-@RefreshScope
-@RestController
-public class ProductClientController {
+public class ProductClientController extends AbstractClient {
 	
-    @Autowired
-    private RestTemplate restTemplate;
-    @Autowired
-    private EurekaClient eurekaClient;
-    @Value("${service.productsearch.serviceId}")
-    private String productSearchServiceId;
+    @Value("${service.productSearch.serviceId}")
+    private String searchServiceId;
     
-    @HystrixCommand(fallbackMethod = "fallbackProdById")
-    @RequestMapping("/order/product/{productId}")
+    private final String TYPE_KEY = "product";
+    
+    @HystrixCommand(fallbackMethod = "fallbackById")
+    @RequestMapping("/order/product/{Id}")
     public Product find(@PathVariable Long productId) {
-        Application application = eurekaClient.getApplication(productSearchServiceId);
-        InstanceInfo instanceInfo = application.getInstances().get(0);
-        String url = "http://" + instanceInfo.getIPAddr() + ":" + instanceInfo.getPort() + "/" + "product/read/" + productId;
-        System.out.printf("URL: %s\n",url);
-        Product prod = restTemplate.getForObject(url, Product.class);
-        System.out.printf("RESPONSE: %s\n",prod);
-        return prod;
+
+        return (Product)super.find(productId);
     }
         
-    @HystrixCommand(fallbackMethod = "fallbackAllProducts")
+    @HystrixCommand(fallbackMethod = "fallbackAll")
     @RequestMapping("/order/product/all")
     public Collection <Product> findProducts() {
-        Application application = eurekaClient.getApplication(productSearchServiceId);
-        InstanceInfo instanceInfo = application.getInstances().get(0);
-        String url = "http://" + instanceInfo.getIPAddr() + ":" + instanceInfo.getPort() + "/" + "product/all";
-        System.out.printf("URL: %s\n", url);
-        
-		Collection<Product> list =  restTemplate.getForObject(url, ProductCollection.class);
-        System.out.printf("RESPONSE: %s\n", list);
-        return list;
+
+		return convertModelList(super.findAll(), Product.class);
     }
     
-    public Product fallbackProdById() {
+    public final Product fallbackById() {
         return new Product();
     }
     
-    public Collection<Product> fallbackAllProducts() {
+    public final Collection<Product> fallbackAll() {
         return new ArrayList<Product>();
     }    
-	private final class ProductCollection extends ArrayList<Product>{
-    	private static final long serialVersionUID = 7056106857753243257L;
-
-    }
+    
+    public final String getTypeKey() {
+    	return TYPE_KEY;
+    };
+    
+    public final String getSearchServiceId() {
+    	return searchServiceId;
+    };
+    
 }
-
 
